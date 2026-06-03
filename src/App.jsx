469f228396a215a44432a7a307eca63b8976cad3
diyarim123 <diyarimohammed33@gmail.com>
2026-06-03 21:50:30 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { RouterProvider } from "@tanstack/react-router"
+import router from "./router/routes"
+
+function App() {
+  return (
+      <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+import './index.css'
+
+const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
